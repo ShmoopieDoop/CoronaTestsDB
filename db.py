9f1712f70316abdc3f1f1fb967a62aa9870f005e
Tom Malhi <tommalhi1@gmail.com>
2022-01-27 14:56:03 +0200

    changed mongo link to correct db

--- db.py
+++ db.py
@@ -2,7 +2,7 @@ import pymongo
 import os
 
 mongo_client = pymongo.MongoClient(
-    f"mongodb+srv://ShmoopieDoop:{os.environ.get('MONGO_USER_PASSWORD')}@cluster0.ylm9c.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
+    f"mongodb+srv://ShmoopieDoop:{os.environ.get('MONGO_USER_PASSWORD')}@cluster0.ylm9c.mongodb.net/testLocations?retryWrites=true&w=majority"
 )
 db = mongo_client["testLocations"]
 colec = db["testLocations"]
